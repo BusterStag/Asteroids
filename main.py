167ad374958ba6a9b9c38c239710d0cf0d5bfa26
generousmouse40 <ryanlisenby@msn.com>
2025-01-25 20:49:43 -0800

    Added movement and groups (drawable and updatable)

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
+
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	while True:
-		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        			return
-		pygame.display.flip()
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        dt = clock.tick(60)/1000
+        for item in updatable:
+            item.update(dt)
+        screen.fill("black")
+        for dr in drawable:
+            dr.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
-	main()
+    main()
