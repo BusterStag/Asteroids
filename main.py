15ae654f63bc9666c8219b60a2f6e2e2d5ab430a
generousmouse40 <ryanlisenby@msn.com>
2025-01-28 22:35:00 -0800

    Added asteroid interaction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     print("Starting asteroids!")
@@ -10,10 +13,19 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -23,6 +35,14 @@ def main():
         for item in updatable:
             item.update(dt)
         screen.fill("black")
+        for ast in asteroids:
+            if ast.collision(player) == True:
+                raise SystemExit("Game Over")
+        for ast in asteroids:
+            for s in shots:
+                if s.collision(ast) == True:
+                    Asteroid.split(ast)
+                    pygame.sprite.Sprite.kill(s)
         for dr in drawable:
             dr.draw(screen)
         pygame.display.flip()
